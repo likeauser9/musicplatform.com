6a57c4b925299d20e0045abd9e39c10ed86d77ec
likeauser9 <shinrinyoku0001@gmail.com>
2025-02-05 14:53:53 +0100

    ну типа коммит

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const nav_menu = document.querySelector(".nav-menu");
+let menu_list = document.getElementById("menu_list");
+
+menu_list.style.maxHeight = "0px";
+
+function toggle_menu() {
+  if (menu_list.style.maxHeight == "0px") {
+    menu_list.style.maxHeight = "700px";
+  } else {
+    menu_list.style.maxHeight = "0px";
+  }
+}
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
