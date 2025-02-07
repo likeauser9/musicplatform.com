14f67a47059fc1b5cd131cfa37866f1cfa4732ed
likeauser9 <shinrinyoku0001@gmail.com>
2025-02-07 18:43:13 +0100

    типа коммит

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const nav_menu = document.querySelector(".nav-menu");
 let menu_list = document.getElementById("menu_list");
+// Flip Button
+document.querySelectorAll(".concert-card-container").forEach((card) => {
+  const moreBtn = card.querySelector(".more-btn");
+  const backBtn = card.querySelector(".back-btn");
+
+  moreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    card.classList.add("flipped");
+  });
+
+  backBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    card.classList.remove("flipped");
+  });
+});
 
 menu_list.style.maxHeight = "0px";
 
